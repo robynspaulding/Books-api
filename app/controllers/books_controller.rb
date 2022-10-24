7368c597a0128ed30d0e6527ce6786b40db9a7c8
RobynSpaulding <robynm27@gmail.com>
2022-10-24 11:56:17 -0700

    implements index, create, show, update and destory actions in books controller, ceates request.http file for testing funcionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,44 @@
 class BooksController < ApplicationController
+
+  def index
+    books = Book.all
+    render json: books.as_json
+  end
+
+  def create
+    book = Book.new(
+      title: params[:title],
+      author: params[:author],
+      description: params[:description],
+      language: params[:language]
+    )
+
+    if book.save
+      render json: book.as_json
+    else
+      render json: {message: "didn't save"}
+    end
+  end
+
+  def show
+    book = Book.find_by(id: params[:id])
+    render json: book.as_json
+  end
+
+  def update
+    book = Book.find_by(id: params[:id])
+    book.title = params[:title] || book.title
+    book.author = params[:author] || book.author
+    book.description = params[:description] || book.description
+    book.language = params[:language] || book.language
+    book.save
+    render json: book.as_json
+  end
+
+  def destroy
+    book = Book.find_by(id: params[:id])
+    book.destroy
+
+    render json: {message: "Book successfully deleted"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get "/books" => "books#index"
+  post "/books" => "books#create"
+  get "/books/:id" => "books#show"
+  patch "/books/:id" => "books#update"
+  delete "/books/:id" => "books#destroy"
 end
